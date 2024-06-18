8323c512a0506bb06a44966563df0f973f87464b
Daniel Fekete <danielf0804@gmail.com>
2024-06-18 18:31:20 +0200

    oprava

--- laravel-vue/app/Http/Controllers/UserRegistrationController.php
+++ laravel-vue/app/Http/Controllers/UserRegistrationController.php
@@ -62,10 +62,3 @@ class UserRegistrationController extends Controller
     }
 }
 
-
-
-
-
-
-
-
--- laravel-vue/routes/web.php
+++ laravel-vue/routes/web.php
@@ -3,11 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProgramController;
 use App\Http\Controllers\AdminRegistrationController;
+use App\Http\Controllers\UserRegistrationController;
 use App\Http\Controllers\AdminLoginController;
 use App\Http\Controllers\SpeakerController;
 use App\Http\Controllers\GalleryController;
 use App\Http\Controllers\SponsorController;
-use App\Http\Controllers\ReviewController;
 
 
 Route::get('/{any}', function () {
@@ -17,10 +17,12 @@ Route::get('/{any}', function () {
 Route::post('/admin/register', [AdminRegistrationController::class, 'register']);
 Route::post('/admin/login', [AdminLoginController::class, 'login']);
 
+Route::post('/registracia', [UserRegistrationController::class, 'register']);
+
 Route::get('/api/admin/speakers', [SpeakerController::class, 'index']);
 Route::post('/admin/speakers', [SpeakerController::class, 'store']);
-Route::put('/api/admin/speakers/{speaker}', [SpeakerController::class, 'update']); 
-Route::delete('/admin/speakers/{speaker}', [SpeakerController::class, 'destroy']); 
+Route::put('/api/admin/speakers/{speaker}', [SpeakerController::class, 'update']);
+Route::delete('/admin/speakers/{speaker}', [SpeakerController::class, 'destroy']);
 Route::get('/api/admin/speakers/{speaker}', [SpeakerController::class, 'show']);
 
 Route::get('/api/admin/programs', [ProgramController::class, 'index']);
@@ -34,22 +36,3 @@ Route::get('/api/gallery', [GalleryController::class, 'index']);
 
 Route::post('/admin/sponsors', [SponsorController::class, 'store']);
 Route::get('/api/sponsors', [SponsorController::class, 'index']);
-
-Route::post('/admin/reviews', [ReviewController::class, 'store']);
-Route::get('api/admin/reviews', [ReviewController::class, 'index']);
-Route::get('api/admin/reviews/{review}', [ReviewController::class, 'show']);
-Route::put('api/admin/reviews/{review}', [ReviewController::class, 'update']);
-Route::delete('/admin/reviews/{review}', [ReviewController::class, 'destroy']);
-
-
-
-
-
-
-
-
-
-
-
-
-
